3c978f299ceba1172a84a213cc57a2f31606b2cd
Dimitris Greasidis <dgreasidis@gmail.com>
2019-06-06 19:46:54 +0300

    feat: Pull, Create, Start, Stop, Delete are ready

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController, AlertController, LoadingController, ModalController } from 'ionic-angular';
-import { ContainerComponent } from "../../components/container/container";
 import { ApiProvider } from "../../providers/api/api";
 
 import { ModalPage } from '../modal/modal';
@@ -14,44 +13,41 @@ export class HomePage {
   containers: Array<any> = [];
   images: Array<any> = [];
   loader: any = null;
+  load: boolean = false;
+
+  container_to_show_stats: any = null;
+  container_to_show_logs: any = null;
 
   constructor(public navCtrl: NavController, public api: ApiProvider, public modalCtrl: ModalController, public loadingCtrl: LoadingController, private alertCtrl: AlertController) {
-    // this.containers = [
-    //  { name: "Container 1", prop: "" },
-    //  { name: "Container 2", prop: "" },
-    //  { name: "Container 3", prop: "" },
-    // ];
-    // setInterval(() => {
-    //     this.getAvailableContainers();
-    // }, 3000);
     this.getAvailableContainers();
-
     this.getAvailableImages();
 
+    setInterval(() => {
+        this.getAvailableContainers();
+    }, 10000);
   }
 
-  addContainer() {
-    let cont = { name: "Container 1", prop: "" };
-    this.add(cont);
-  }
-
-  add(container) {
-    this.containers.push(container);
-  }
+///////////////////////////////////////////////////////////////////////////////////
 
   getAvailableContainers() {
-    this.presentLoading();
-
+    this.load = true;
+    // this.presentLoading();
     this.api.getListOfContainers().subscribe(res => {
         // console.log(JSON.stringify(res));
         this.containers = [];
         res.map(cont => {
             this.containers.push(cont);
         });
-        this.dismissInitLoader();
+        setTimeout(() => {
+          this.load = false;
+        }, 2000);
+        // this.dismissInitLoader();
     },
       err => {
-        this.dismissInitLoader();
+        setTimeout(() => {
+          this.load = false;
+        }, 2000);
+        // this.dismissInitLoader();
         // console.log("ERROR while getListOfContainers: " + JSON.stringify(err));
         let msg = "Error while getting list of containers. Try again.";
         this.alertGlobal(msg);
@@ -59,19 +55,14 @@ export class HomePage {
   }
 
   getAvailableImages() {
-    // this.presentLoading();
-
     this.api.getListOfImages().subscribe(res => {
-        // console.log(JSON.stringify(res));
+        console.log("Getting available IMAGES");
         this.images = [];
         res.map(img => {
             this.images.push(img);
         });
-        // console.log(JSON.stringify(this.images))
-        // this.dismissInitLoader();
     },
       err => {
-        // this.dismissInitLoader();
         // console.log("ERROR while getListOfContainers: " + JSON.stringify(err));
         let msg = "Error while getting list of images. Try again.";
         this.alertGlobal(msg);
@@ -79,18 +70,20 @@ export class HomePage {
   }
 
   createContainerModal() {
-      let modal = this.modalCtrl.create(ModalPage, {
-          images: this.images
-      });
+    let modal = this.modalCtrl.create(ModalPage, {
+        images: this.images
+    });
 
-      modal.present();
+    modal.onDidDismiss(data => {
+      // console.log(JSON.stringify(data));
+      if (data) {
+        this.getAvailableImages();
+      }
+      this.getAvailableContainers();;
+    });
+
+    modal.present();
   }
-
-  pullImageModal() {
-
-  }
-
-
 
 ////////////////////////////// PAGE HELPERS ////////////////////////////////////////
 
--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ViewController, LoadingController, Events, AlertController } from 'ionic-angular';
+import { NavController, NavParams, ViewController, LoadingController, Events } from 'ionic-angular';
 
 import { ApiProvider } from '../../providers/api/api';
 
@@ -9,19 +9,40 @@ import { ApiProvider } from '../../providers/api/api';
   templateUrl: 'modal.html'
 })
 export class ModalPage {
+  loader: any = null;
   images: any = null;
+  image_name: any = null;
+  load_images: boolean = false;
 
-
-  constructor(public navCtrl: NavController, public events: Events, private loadingCtrl: LoadingController, private alertCtrl: AlertController, private api: ApiProvider, public navParams: NavParams, public viewCtrl: ViewController) {
+  constructor(public navCtrl: NavController, public events: Events, private loadingCtrl: LoadingController, private api: ApiProvider, public navParams: NavParams, public viewCtrl: ViewController) {
     this.images = this.navParams.get('images');
   }
 
-  createContainer() {
-    console.log("Creating Container");
+  createContainer(img_name) {
+    // console.log(JSON.stringify(img_name.toString()));
+    this.api.createContainer(img_name.toString()).subscribe(res => {
+        this.loader = false;
+        this.dismiss(this.load_images);
+    });
   }
 
-  dismiss() {
-    this.viewCtrl.dismiss();
+  pullImage() {
+    this.load_images = true;
+    this.presentLoading();
+    // console.log("Will pull: " + this.image_name);
+    this.api.pullImage(this.image_name).subscribe(res => {
+        // console.log('Pull img: ' + JSON.stringify(res));
+        this.dismissLoader();
+        this.dismiss(this.load_images);
+    },
+      err => {
+        this.dismissLoader();
+        this.dismiss(this.load_images);
+    });
+  }
+
+  dismiss(data) {
+    this.viewCtrl.dismiss(data);
   }
 
   presentLoading() {
@@ -30,6 +51,13 @@ export class ModalPage {
     });
 
     this.loader.present();
+  }
+
+  dismissLoader() {
+    if (this.loader) {
+      this.loader.dismiss();
+      this.loader = null;
+    }
   }
 
 }
