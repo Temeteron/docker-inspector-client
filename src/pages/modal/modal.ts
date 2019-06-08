06bd54d6f5685a67b21d2130450a61449c6360c6
Dimitris Greasidis <dgreasidis@gmail.com>
2019-06-08 13:54:42 +0300

    handle better responses from pull, create

--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -12,7 +12,9 @@ export class ModalPage {
   loader: any = null;                // LOADER VAR FOR ASYNC REQUESTS
   images: any = null;                // ARRAY THAT CONTAINS AVAILABLE IMAGES
   image_name: any = null;            // VARIABLE TO SAVE IMAGE NAME TO PULL
-  load_images: boolean = false;      // BOOLEAN IF IMAGE WAS PULLED = TRUE
+
+  // EXAMPLE REPO NAMES TO PULL IMAGE
+  example_images: Array<string> = ['chentex/random-logger:latest', 'enhariharan/infinite-loop:latest', 'hello-world:latest'];
 
   constructor(private loadingCtrl: LoadingController, private api: ApiProvider, public navParams: NavParams, public viewCtrl: ViewController) {
     this.images = this.navParams.get('images');
@@ -22,13 +24,41 @@ export class ModalPage {
 // CREATE CONTAINER FROM GIVER IMAGE
 ///////////////////////////////////////////////////////////////////////////
   createContainer(img_name) {
+    // BECAUSE A NEW CONTAINER WAS CREATED INFORM PARENT TO 
+    // GET THE NEW LIST OF CONTAINERS
+    let objMsg = {
+      'load_images': false,
+      'load_containers': true,
+      'message': ''
+    };
+
     // SHOW LOADER
     this.presentLoading();
     this.api.createContainer(img_name.toString()).subscribe(res => {
+        // DEBUG MESSAGE
+        console.log('Res createContainer: ' + JSON.stringify(res));
         // DISMISS LOADER
         this.dismissLoader();
+
+        // MESSAGE TO SHOW
+        objMsg.message = 'Container created';
+
         // DISMISS MODAL
-        this.dismiss(this.load_images);
+        this.dismiss(objMsg);
+    },
+      err => {
+        // DEBUG MESSAGE
+        console.error('Error createContainer: ' + JSON.stringify(err));
+        // DISMISS LOADER
+        this.dismissLoader();
+
+        // ERROR MESSAGE TO SHOW
+        objMsg.message = `Error ${err.status}: ${err.statusText}`;
+        // CHECK IF SERVER IS DOWN, IF YES DONT MAKE FURTHER REQUESTS
+        objMsg.load_containers = err.status == 0 ? false : true;
+
+        // DISMISS MODAL
+        this.dismiss(objMsg);
     });
   }
 
@@ -36,24 +66,49 @@ export class ModalPage {
 // PULL IMAGE FROM REPOSITORY
 ///////////////////////////////////////////////////////////////////////////
   pullImage() {
-    // BECAUSE IMAGE WAS PULLED INFORM PARENT TO 
+    // BECAUSE A NEW IMAGE WAS PULLED INFORM PARENT TO 
     // GET THE NEW LIST OF IMAGES
-    this.load_images = true;
+    let objMsg = {
+      'load_images': true,
+      'load_containers': false,
+      'message': ''
+    };
 
     // SHOW LOADER
     this.presentLoading();
     this.api.pullImage(this.image_name).subscribe(res => {
+        // DEBUG MESSAGE
+        console.log('Res pullImage: ' + JSON.stringify(res));
         // DISMISS LOADER
         this.dismissLoader();
+
+        // CHECK IF THERE IS A WARNING AND RETURN MESSAGE
+        objMsg.message = (res.status == 'Warning') ? (`Warning. ${res.message}`) : (res[res.length-1].status);
+
         // DISMISS MODAL
-        this.dismiss(this.load_images);
+        this.dismiss(objMsg);
     },
       err => {
+        // DEBUG MESSAGE
+        console.error('Error pullImage: ' + JSON.stringify(err));
         // DISMISS LOADER
         this.dismissLoader();
+
+        // ERROR MESSAGE TO SHOW
+        objMsg.message = `Error: ${err.status}. ${err.statusText}`;
+        // CHECK IF SERVER IS DOWN, IF YES DONT MAKE FURTHER REQUESTS
+        objMsg.load_images = err.status == 0 ? false : true;
+
         // DISMISS MODAL
-        this.dismiss(this.load_images);
+        this.dismiss(objMsg);
     });
+  }
+
+///////////////////////////////////////////////////////////////////////////
+// SELECT IMAGE TO PULL
+///////////////////////////////////////////////////////////////////////////
+  selectImageToPull(name) {
+    this.image_name = name;
   }
 
 ///////////////////////////////////////////////////////////////////////////
